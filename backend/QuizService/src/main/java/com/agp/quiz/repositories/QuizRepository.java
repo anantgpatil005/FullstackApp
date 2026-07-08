b1e5f254fcff068d511ca1de55b239be9c602af1
anantgpatil005 <anantgpatil005@gmail.com>
2026-07-08 18:10:53 +0530

    removed irrelavant code and config changes specific to docker

--- backend/QuizService/src/main/java/com/agp/quiz/repositories/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agp.quiz.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agp.quiz.entities.Quiz;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-
-}
